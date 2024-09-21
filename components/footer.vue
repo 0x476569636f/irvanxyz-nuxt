2000ac9d771d5ecf00797329ce989c19ef502f09
0x476569636f <darknet1255@gmail.com>
2024-09-21 07:32:56 +0700

    feat: update footer to include nuxt.js logo link

--- components/footer.vue
+++ components/footer.vue
@@ -2,7 +2,10 @@
   <footer
     class="mt-auto block px-4 pb-2 pt-4 text-sm text-zinc-500 dark:text-green-300"
   >
-    @{{ new Date().getFullYear() }} made with nuxt
+    @{{ new Date().getFullYear() }} made with
+    <a href="https://nuxt.com/" target="_blank" class="ml-0.5"
+      ><UIcon name="logos:nuxt-icon"></UIcon
+    ></a>
   </footer>
 </template>
 
